bffce84b3188fb5952e84ed637b38ac30dd14488
卜得庆 <budeqing@bodeqingdeMacBook-Pro.local>
2023-03-20 16:37:10 +0800

    feat: init

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const CracoLessPlugin = require('craco-less');
+
+const resolve = (pathname) => path.resolve(__dirname, pathname);
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+    },
+  ],
+  webpack: {
+    alias: {
+      '@': resolve('src'),
+    },
+  },
+};
